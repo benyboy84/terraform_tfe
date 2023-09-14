8ee9d53c203a518172d09ba0cc97a8b4f1c2c26e
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-09-14 12:32:50 -0400

    test

--- resources_module.tf
+++ resources_module.tf
@@ -6,11 +6,11 @@ module "modules" {
   for_each = local.modules
 
   module_provider = try(each.value.module_provider, null)
-  name            = try(each.value.name, null)
-  organization    = try(each.value.organization, null)
-  namespace       = try(each.value.namespace, null)
-  registry_name   = try(each.value.registry_name, null)
-  vcs_repo        = try(each.value.vcs_repo, null)
+  name = try(each.value.name, null)
+  organization = try(each.value.organization, null)
+  namespace = try(each.value.namespace, null)
+  registry_name = try(each.value.registry_name, null)
+  vcs_repo = try(each.value.vcs_repo, null)
 
 }
 
